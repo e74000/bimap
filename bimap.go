621b6a12c60051fe21a65ea3876cf64a930304b3
e74000 <olimcinnes@protonmail.com>
2022-11-28 11:10:58 +0000

    Prevent changing keys from leaving hanging values

--- bimap.go
+++ bimap.go
@@ -17,24 +17,6 @@ func MapToBimap[P comparable, Q comparable](m map[P]Q) *BiMap[P, Q] {
 	return bm
 }
 
-// MapToBimapOdd is so that you can have a one <-> many relationship for a few items
-// it does not guarantee that the map is symmetric, however.
-func MapToBimapOdd[P comparable, Q comparable](m map[P]Q) *BiMap[P, Q] {
-	bm := NewBiMap[P, Q]()
-
-	for p, q := range m {
-		if bm.ExistsRev(q) {
-			bm.setFor(p, q)
-		} else if bm.ExistsFor(p) {
-			bm.setRev(p, q)
-		} else {
-			bm.Set(p, q)
-		}
-	}
-
-	return bm
-}
-
 type BiMap[P comparable, Q comparable] struct {
 	forward map[P]Q
 	reverse map[Q]P
@@ -61,15 +43,11 @@ func (b *BiMap[P, Q]) DelRev(q Q) {
 }
 
 func (b *BiMap[P, Q]) Set(p P, q Q) {
-	b.forward[p] = q
-	b.reverse[q] = p
-}
+	if b.ExistsFor(p) {
+		b.DelFor(p)
+	}
 
-func (b *BiMap[P, Q]) setFor(p P, q Q) {
 	b.forward[p] = q
-}
-
-func (b *BiMap[P, Q]) setRev(p P, q Q) {
 	b.reverse[q] = p
 }
 
